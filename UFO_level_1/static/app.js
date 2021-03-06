2e7a1ebe28524340a20c14190c2fd4ce972e6e46
LaTisha Habersham <72284231+llhabers@users.noreply.github.com>
2021-03-06 08:08:05 -0500

    updated work

---
+++ UFO_level_1/static/app.js
@@ -0,0 +1,8 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE
+//1. Populate the html file with the data in the form of table(refer activity Class 3 - 03)
+//2. Pulling in the data from the form field(refer activity Class 3 - 08)
+//3. Filter the data.js using this pulled value(refer activity CLass 3 -09)
+//4. Repopulate the table with this filtred data(refer activity Class 3 - 03)
